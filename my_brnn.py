17e6b7aa29378be4d8c02d940b05d18ff80ea578
odellus <thomas@wgapl.com>
2015-12-23 08:46:00 -0800

    :lipstick:sleeker, faster, sexier

--- my_brnn.py
+++ my_brnn.py
@@ -77,11 +77,8 @@ class BRNNLayer:
         n_in = self.n_in
         n_hidden = self.n_hidden
         n_out = self.n_out
-        T = X.shape[1]
+        T = X.shape[1] # length of input/output sequence
 
-        b_f = self.bias[:n_hidden]
-        b_b = self.bias[n_hidden:2*n_hidden]
-        b_y = self.bias[2*n_hidden:]
         if X.shape[0] != n_in:
             return "Size of feature space of data and network disagree"
 
@@ -90,47 +87,54 @@ class BRNNLayer:
 
         # Initializing hidden state matrix
         H = np.zeros((2*n_hidden,T))
-        H[:n_hidden,0] = tanh(Wx[:n_hidden,0]+b_f)
-        H[n_hidden:,T-1] = tanh(Wx[n_hidden:,T-1]+b_b)
+        H[:n_hidden,0] = tanh(Wx[:n_hidden,0]+self.bias[:n_hidden])
+        H[n_hidden:,T-1] = tanh(Wx[n_hidden:,T-1]+self.bias[n_hidden:2*n_hidden])
         for k in range(1,T):
             H[:n_hidden,k] = tanh(Wx[:n_hidden,k] + \
-            np.dot(self.W_h[:n_hidden,:],H[:n_hidden,k-1]) + b_f)
-            print T-k+1
+            np.dot(self.W_h[:n_hidden,:],H[:n_hidden,k-1]) + self.bias[:n_hidden])
+
             H[n_hidden:,T-k-1] = tanh(Wx[n_hidden:,T-k-1] + \
-            np.dot(self.W_h[n_hidden:,:],H[:n_hidden,T-k]) + b_b)
+            np.dot(self.W_h[n_hidden:,:],H[:n_hidden,T-k]) + self.bias[n_hidden:2*n_hidden])
 
-        B_y = np.tile(b_y, (T,1)).T # create a n_out X T matrix
-
-        # G for Guess
-        print H.shape
-        print self.W_y.shape
-        return np.dot(self.W_y,H) + B_y
+        return np.dot(self.W_y,H) + np.tile(self.bias[2*n_hidden:], (T,1)).T
 
 
 def rudimentary_test():
+    """
+    Very simple test of BRNNLayer functionality. I'm training a DQN for
+    Space Invaders right now and I don't really want to get into any training
+    until my GPU is free for all the matrix multiplication.
+    """
+
     s = """0 a is the quick fox who jumped over the lazy brown dog's new sentence."""
     table = gen_bag_hashtable()
+
     v = make_wordvector(s, table)
-    s0 = make_string(v)
-    # print s0
-    # print v
+
     n_in, T = v.shape
     n_out = n_in
     n_hidden = 100 # Learn a more complex representation?
-    eps = 0.01
-    print n_in, T
+    eps = 0.1
+
 
     n_params = 2*n_in*n_hidden + \
     2*n_hidden*n_hidden + \
     2*n_out*n_hidden + \
     2*n_hidden+n_out
 
-    params1 = random(n_params,)
+    params1 = eps*(2*random(n_params,)-1.)
+    params2 = eps*(2*random(n_params,)-1.)
+    params3 = eps*(2*random(n_params,)-1.)
 
     brnn1 = BRNNLayer(n_in,n_hidden,n_in,params1, eps)
+    brnn2 = BRNNLayer(n_in,n_hidden,n_in,params2, eps)
+    brnn3 = BRNNLayer(n_in,n_hidden,n_in,params3, eps)
 
-    g = brnn1.gen_sequence(v)
-    s1 = make_string(g)
+    y1 = brnn1.gen_sequence(v)
+    y2 = brnn2.gen_sequence(y1)
+    y3 = brnn3.gen_sequence(y2)
+
+    s1 = make_string(y3)
     print s1
 
 if __name__ == "__main__":
